cb51552b70a9025dc92cf2519a4280184bd59d24
Daseul <dls2tmfs2@gmail.com>
2021-06-04 09:31:18 +0900

    Modify : 최종수정

--- src/Routes.js
+++ src/Routes.js
@@ -6,23 +6,19 @@ import Nav from './Components/Nav';
 import List from './Pages/List/List';
 import Footer from './Pages/Main/Components/MainFooter';
 import DetailDrivers from './Pages/DetailTaxiDriver/DetailTaxiDriver';
+import Reserve from './Pages/Reserve/Reserve';
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
-        <Nav />
         <Switch>
           <Route exact path="/" component={Main} />
           <Route exact path="/login" component={Login} />
-          <Route
-            exact
-            path="/list/:departure/:arrival/:date/:seat"
-            component={List}
-          />
+          <Route exact path="/list" component={List} />
           <Route exaxt path="/detail/:id" component={DetailDrivers} />
+          <Route exact path="/reserve" component={Reserve} />
         </Switch>
-        <Footer />
       </Router>
     );
   }
