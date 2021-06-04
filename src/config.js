e7c3e81ea91dfd0017c6012660160790ed69675d
Kim Yeseul <96linda@hanmail.net>
2021-06-04 09:48:34 +0900

    Modify: 택시 검색결과 리스트페이지
    - 리스트페이지 추가
    - 검색결과 동적라우팅
    - 필터, 정렬 기능 추가
    - 티켓 선택, 예약 기능 추가
    - 달력, input range 라이브러리 추가, 공용 함수 util.js 추가
    - api test 완료

--- src/config.js
+++ src/config.js
@@ -1,8 +1,13 @@
 const IP = 'http://3.34.199.216:8000';
+
 export const API = {
-  STATION: `${IP}`,
-  STATION_TEST: `${IP}/data/station.json`,
+  STATION: `${IP}/taxis/locations`,
+  STATION_TEST: '/data/station.json',
+  COURSE: `${IP}/taxis`,
+  ORDER: `${IP}/orders`,
   KAKAO_SIGNIN_AND_SIGNUP: `${IP}/users/kakaosignin`,
   TAXI_DRIVER_DETAIL: `${IP}/taxis/taxidrivers`,
   TAXI_DRIVER_COMMENT: `${IP}/taxis/reviews`,
+  LOGOUT: `${IP}/users/logout`,
+  USER_INFO: `${IP}/users/userinfo`,
 };
